dd8edc07922f667d71689029256cef1034086ed0
Daltonshults <Dshults@cocc.edu>
2025-10-27 11:31:55 -0700

    Removed default code, and added Header.

--- my-site/src/App.tsx
+++ my-site/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import React from 'react'
+import Header from './components/header'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <p>Hello world!</p>
+      <Header />
+  
     </>
   )
 }
