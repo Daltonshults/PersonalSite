dbc5eab45564d8015aec36625afb04a5f4024ef6
Daltonshults <Dshults@cocc.edu>
2025-11-04 09:44:49 -0800

    Added AboutMe to app.

--- my-site/src/App.tsx
+++ my-site/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import React from 'react'
+
 import Header from './components/header'
+import AboutMe from './components/aboutme'
+
 import './App.css'
 
 function App() {
@@ -8,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <AboutMe />
   
     </>
   )
