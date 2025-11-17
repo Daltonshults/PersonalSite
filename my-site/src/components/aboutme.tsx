9efc2fd6de8144b319d7fd7d8246f87e38fcba95
Daltonshults <Dshults@cocc.edu>
2025-11-17 14:19:20 -0800

    Added custom text options.

--- my-site/src/components/aboutme.tsx
+++ my-site/src/components/aboutme.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useState } from 'react';
+import LargeText from "./largeText";
+import SmallText from "./smallText";
 
 
 const AboutMe: React.FC = () =>  {
+
+    const [hovered, setHovered] = useState<boolean>(false);
 
     return (
         <div
@@ -25,6 +30,8 @@ const AboutMe: React.FC = () =>  {
             }
             >
                 <img 
+                onMouseEnter = {() => setHovered(true)}
+                onMouseLeave = {() => setHovered(false)}
                 style={
                     {
                         borderRadius: "50%",
@@ -35,7 +42,7 @@ const AboutMe: React.FC = () =>  {
 
                     }
                 }
-                src="https://mir-s3-cdn-cf.behance.net/project_modules/hd_webp/108089180721037.650fb50bd637a.png" alt="" />
+                src= {hovered ? "https://mir-s3-cdn-cf.behance.net/project_modules/hd_webp/108089180721037.650fb50bd637a.png" : "https://i.ytimg.com/vi/C0cALbLd70o/maxresdefault.jpg" } alt="" />
             </div>
             <div
             style={
@@ -47,8 +54,17 @@ const AboutMe: React.FC = () =>  {
                 }
             }
             >
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius facere magnam impedit nesciunt rem tenetur nam consequuntur, blanditiis voluptatem magni nulla cum dicta ipsum, voluptas corrupti, alias iusto dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam et corporis corrupti porro exercitationem nostrum laboriosam. Porro adipisci sed, nobis illum deserunt quas amet autem!</p>
+                <LargeText
+                >About Me
+                </LargeText>
+
+                <SmallText>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius facere magnam impedit nesciunt rem tenetur nam consequuntur, blanditiis voluptatem magni nulla cum dicta ipsum, voluptas corrupti, alias iusto dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam et corporis corrupti porro exercitationem nostrum laboriosam. Porro adipisci sed, nobis illum deserunt quas amet autem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi facere doloremque voluptatem provident. Culpa nobis veniam eius animi temporibus! Rerum, debitis sed? Laboriosam quia distinctio corporis doloremque asperiores accusamus numquam?
+                </SmallText>
+
+                <SmallText>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo ipsa sapiente sunt magnam veniam sed voluptatibus distinctio aut! Similique minima nesciunt deleniti dolores, iure in corporis suscipit illum quasi quaerat!
+                </SmallText>
             </div>
 
         </div>
