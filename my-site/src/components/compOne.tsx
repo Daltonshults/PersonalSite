2f99a4dbeaedaea063b9ea051f694c42bb75943c
Daltonshults <Dshults@cocc.edu>
2025-11-17 15:23:19 -0800

    Changed component to more accurately reflect what the content will be.

--- my-site/src/components/compOne.tsx
+++ my-site/src/components/compOne.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ComponentOne: React.FC = () => {
-    return <h1>Component One</h1>
+    return (
+    <div>
+    <h1>Component One</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsum omnis aliquid corporis neque iure beatae, qui libero optio ad hic, vel eligendi ratione. Quaerat aspernatur a cumque quas nobis?</p>
+    </div>
+    )
 }
 
 export default ComponentOne;
