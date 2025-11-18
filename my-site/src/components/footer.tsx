f5e198e994b11f779a70eb5f1eef63a3ad54d7b7
Daltonshults <Dshults@cocc.edu>
2025-11-18 15:36:37 -0800

    Adjusted layout of div

--- my-site/src/components/footer.tsx
+++ my-site/src/components/footer.tsx
@@ -2,7 +2,20 @@ import React from "react";
 
 
 const Footer: React.FC = () => {
-    return <div>hello</div>
+    return (
+        <div
+            style={
+                {
+                    height: "20rem",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                }
+            }
+        >
+            <h1>Hello World!</h1>
+        </div>
+    )
 };
 
 export default Footer;
