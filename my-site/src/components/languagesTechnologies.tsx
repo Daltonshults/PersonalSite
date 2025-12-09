cb68ad2b43f93588f87ba6e1b9e80eea18bdb3ac
Daltonshults <Dshults@cocc.edu>
2025-12-09 15:01:40 -0800

    Created langtech file.

---
+++ my-site/src/components/languagesTechnologies.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { resumeData } from "../data/resume";
+import BulletPoints from './bulletPoints';
+
+const languagesTechnologies: React.FC = () => {
+    
+    return (
+        <div
+        style={
+            {
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+                margin: "2.5rem 5rem 2.5rem 5rem"
+            }
+        }
+    >
+    </div>
+    )
+}
+
+export default languagesTechnologies;
