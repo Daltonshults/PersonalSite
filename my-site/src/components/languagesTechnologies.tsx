934e35c180b5b05a1dfe29842656f5d65ce128da
Daltonshults <Dshults@cocc.edu>
2025-12-09 16:12:37 -0800

    Adjusted the bulletpoints so that they display almost evenly.

--- my-site/src/components/languagesTechnologies.tsx
+++ my-site/src/components/languagesTechnologies.tsx
@@ -1,20 +1,60 @@
 import React from 'react';
 import { resumeData } from "../data/resume";
 import BulletPoints from './bulletPoints';
+import ResumeHeader from './resumeHeader';
 
 const languagesTechnologies: React.FC = () => {
-    
+
+    const floorLength = Math.floor(resumeData.langtech.length / 3);
+
+    let current: number = 0;
+    let slices: string[][] = [];
+    for (let i = 0; i < 3; i++) {
+        if (i < 2) {
+            slices.push(resumeData.langtech.slice(current, current+floorLength))
+            current += floorLength
+        } else {
+            slices.push(resumeData.langtech.slice(current))
+        }
+    };
+
+    type LangTechBulletsProps = {
+        bullets: string[][]
+    }
+
+    const LangTechBullets: React.FC<LangTechBulletsProps> = ( { bullets} ) => {
+
+        return (
+            <div
+                style={
+                    {
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center", 
+                    }
+                }
+            >
+                {bullets.map( (columnItems, i ) => (
+                    <BulletPoints key={i} items={columnItems}/>
+                ))}
+            </div>
+        )
+    }
+
     return (
         <div
         style={
             {
                 display: "flex",
                 flexDirection: "column",
+                alignItems: "center",
                 height: "100%",
                 margin: "2.5rem 5rem 2.5rem 5rem"
             }
         }
     >
+        <ResumeHeader>Languages and Technologies</ResumeHeader>
+        <LangTechBullets bullets={slices}/>
     </div>
     )
 }
