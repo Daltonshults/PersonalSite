fcb4504f10166be1a21c51194db2dbc4230e4b71
Daltonshults <Dshults@cocc.edu>
2025-12-09 16:13:44 -0800

    Designed the entire coursework section. Needs refactoring.

---
+++ my-site/src/components/courseWork.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { resumeData } from '../data/resume';
+import BulletPoints from './bulletPoints';
+import ResumeHeader from './resumeHeader';
+import SubHeader from './resumeSubHeader';
+import SmallText from './smallText';
+
+const CourseWork: React.FC = () => {
+
+    type CourseWorkItemProps = 
+        {
+            title: string
+            description: string
+        }
+
+
+
+    const CourseworkItem: React.FC<CourseWorkItemProps> = ( { title, description } ) => {
+        return (
+            <div
+                style={
+                    {
+                        margin: ".5rem 5rem .5rem 5rem"
+                    }
+                }
+            >
+                <SubHeader>{title}</SubHeader>
+                <SmallText>{description}</SmallText>                
+            </div>
+        )
+    }
+
+    type CourseWorkProps = {
+        courseWork: CourseWorkItemProps[]
+    }
+
+    const CourseWork: React.FC<CourseWorkProps> = ( { courseWork } ) => {
+        return (
+            <div>
+                { courseWork.map( (exp, i) =>(
+                    <CourseworkItem key={i} title={exp.title} description={exp.description}/>
+                ))}
+            </div>
+        )
+    }
+
+    return (
+        <div
+            style={
+                {
+                    display: "flex",
+                    flexDirection: "column"
+                }
+            }
+        >
+            <ResumeHeader>Course Work</ResumeHeader>
+            <SubHeader style= {{alignSelf: "center"}}>BACHELOR OF COMPUTER SCIENCE | OREGON STATE UNIVERSITY | GPA: 3.97</SubHeader>
+            <CourseWork courseWork={resumeData.courseWork}/>
+
+        </div>
+
+    );
+}
+
+export default CourseWork ;
