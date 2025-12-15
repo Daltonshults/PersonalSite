1c3034e1f0f5443581db8d04d729877c11559f1d
Daltonshults <Dshults@cocc.edu>
2025-12-15 15:04:51 -0800

    Adjusted styling.

--- my-site/src/components/resumeHeader.tsx
+++ my-site/src/components/resumeHeader.tsx
@@ -14,7 +14,7 @@ const ResumeHeader: React.FC<ResumeHeaderProps> = ( {children, style} ) => {
                 {
                     justifySelf: "center",
                     alignSelf: "center",
-                    margin: "5rem 0rem 2.5rem 0rem",
+                    margin: "0rem 0rem .5rem 0rem",
                     ...style
                 }
             }
